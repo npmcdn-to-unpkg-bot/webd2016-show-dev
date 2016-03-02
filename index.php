8c647a67e036414580214f5c9c346d4c7a4f1327
Shawn Santiago <santiagoshawn@gmail.com>
2016-03-02 09:59:26 -0500

    html skeleton readded

--- index.php
+++ index.php
@@ -16,6 +16,34 @@
         <script src="js/script.js"></script>
     </head>
 <body>
-	
+    <div class="container">
+      <section class="header clearfix">
+        <nav>
+          <ul class="nav nav-pills pull-right">
+            <li role="presentation" class="active"><a href="#">Home</a></li>
+            <li role="presentation"><a href="#">About</a></li>
+            <li role="presentation"><a href="#">Contact</a></li>
+          </ul>
+        </nav>
+        <h3 class="text-muted" id="logo">Project name</h3>
+      </section>
+      <section class="intro">
+          
+      </section>
+      <section class="gallery">
+          
+      </section>
+      <section class="map">
+          
+      </section>
+      <section class="rsvp-info">
+          
+      </section>
+      <section class="instagram-feed">
+          
+      </section>
+      <footer>
+      </footer>
+    </div>
 </body>
 </html>
