d84639bcf38f178cde89323f30334c35001b1a64
wesleydean <wdean.francis@gmail.com>
2016-03-02 13:25:58 -0500

    connect firebase

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-"use strict";
+(function(){
+
+	var url = new Firebase('https://webd-portfolioshow.firebaseIO.com');
+	
+	url.child('students').once('value', function(snapshot) {
+	    var i = 0;
+	    
+	    var name = snapshot.val()[i].name;
+	    var bio = snapshot.val()[i].bio;
+	    var website = snapshot.val()[i].website;
+	    console.log(" name: "+ name + ", bio: " + bio + ", website: " + website);
+ 	 });
+
+})();
