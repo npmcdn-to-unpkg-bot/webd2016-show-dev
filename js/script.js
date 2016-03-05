b1bfcad470c52278f883ad7a807e4c831726740b
Anna <anna@safronovs.com>
2016-03-05 14:41:24 -0500

    added footer markup

--- js/script.js
+++ js/script.js
@@ -98,7 +98,7 @@ function initMap() {
   	scrollwheel: false,
   	draggable: false,
     
-    //custom map theme
+    //custom map theme - https://snazzymaps.com
     styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#193341"}]},
     {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2c5a71"}]},
     {"featureType":"road","elementType":"geometry","stylers":[{"color":"#29768a"},{"lightness":-37}]},
