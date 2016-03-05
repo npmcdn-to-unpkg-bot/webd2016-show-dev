61943cb31b02fdb8a708e979117b734691f19205
Anna <anna@safronovs.com>
2016-03-05 14:04:24 -0500

    added colored map

--- js/script.js
+++ js/script.js
@@ -83,5 +83,38 @@
 
 })();
 
+//set up the map
+function initMap() {
+  var myLatLng = {lat: 43.647054, lng: -79.403410};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: myLatLng,
+
+    //map settings
+    disableDefaultUI: true,
+    zoomControl: false,
+  	scaleControl: false,
+  	scrollwheel: false,
+  	draggable: false,
+    
+    //custom map theme
+    styles: [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#193341"}]},
+    {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#2c5a71"}]},
+    {"featureType":"road","elementType":"geometry","stylers":[{"color":"#29768a"},{"lightness":-37}]},
+    {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#406d80"}]},{"featureType":"transit","elementType":"geometry","stylers":
+    [{"color":"#406d80"}]},{"elementType":"labels.text.stroke","stylers":[{"visibility":"on"},{"color":"#3e606f"},{"weight":2},
+    {"gamma":0.84}]},{"elementType":"labels.text.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"administrative","elementType":"geometry","stylers":
+    [{"weight":0.6},{"color":"#1a3541"}]},{"elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry",
+    "stylers":[{"color":"#2c5a71"}]}]
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Hello World!'
+  });
+}
+
 
 
