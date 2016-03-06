97d5fd3ebf1476d2ed33d1d23eeb0349813498da
fumif <fumi@Fumis-MacBook-Pro.local>
2016-03-05 20:42:10 -0500

    commit

--- index.php
+++ index.php
@@ -2,20 +2,67 @@
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang=""> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8" lang=""> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9" lang=""> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-        <title>Portfolio Show</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<!--[if gt IE 8]><!-->
+<html class="no-js" lang="en">
+<!--<![endif]-->
 
-        <link rel="stylesheet" href="css/normalize.min.css">
-        <link rel="stylesheet" href="css/main.css">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <title>Portfolio Show</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="styles/normalize.min.css">
+    <link rel="stylesheet" href="styles/main.css">
+    <script src="js/script.js"></script>
+</head>
 
-        <script src="js/script.js"></script>
-    </head>
 <body>
-	
+    <div class="container">
+        <header>
+            <h1>LOGO</h1>
+        </header>
+        <section>
+            <article id="title">
+                <p>Humber's</p>
+                <h1>Web Design &amp; Interactive Media</h1>
+                <p>Portfolio show</p>
+            </article>
+            <article id="info">
+                <ul>
+                    <li>
+                        <ul id="date">
+                            <li>
+                                <p>April 25th, 2016</p>
+                                <h1>5PM - 8PM</h1>
+                            </li>
+                            <li>
+                                <p>The Burroughes Building 639 Queen Street West Toronto, M5V 2B7</p>
+                            </li>
+                        </ul>
+                    </li>
+                    <li id="newsletter">
+                        Keep an eye out for our full website. Get updates by subscribing below!
+                        <button>NOTIFY ME</button>
+                    </li>
+                </ul>
+            </article>
+    </div>
+    </section>
+    <footer>
+        <ul>
+            <li id="counter">
+                <p>Keep your eyes peeled...</p>
+                COUNTDOWN
+            </li>
+            <li id="social">
+                <ul>
+                    <li>Twitter</li>
+                    <li>Instagram</li>
+                </ul>
+            </li>
+        </ul>
+    </footer>
 </body>
+
 </html>
