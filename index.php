0587e51d84e2476746c6e572249f7f7c1c2d55cd
fumif <fumi@Fumis-MacBook-Pro.local>
2016-03-09 13:40:20 -0500

    Editted desktop version of landing page

--- index.php
+++ index.php
@@ -5,64 +5,91 @@
 <!--[if gt IE 8]><!-->
 <html class="no-js" lang="en">
 <!--<![endif]-->
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+		<title>Portfolio Show</title>
+		<meta name="description" content="">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+		<link rel="stylesheet" href="styles/normalize.min.css">
+		<link rel="stylesheet" href="styles/main.css">
+		<link href="http://cdn-images.mailchimp.com/embedcode/slim-10_7.css" rel="stylesheet" type="text/css">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+		<style type="text/css">
+			#mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+			/* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+			   We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+		</style>
+		<script type="text/javascript" src="https://code.jquery.com/jquery-latest.js"></script> 
+		<script type="text/javascript" src="js/jquery.downCount.js"></script> 
+		<script src="js/script.js"></script>
+	</head>
+		<body>
+			<header class="clearfix">
+				<div class="container">
+					<h1 id="logo">LOGO</h1>
+				</div>
+			</header>
+			<section>
+				<div class="container">
+					<article id="left">
+						<div id="title">
+							<p>Humber College</p>
+							<h1>Web Design &amp; Interactive Media</h1>
+							<p>Portfolio show 2016</p>
+						</div>
+						<div id="info">
+							<div id="when">
+								<div id="date">apr 25<sup>th</sup>, 2016</div>
+								<div id="time">5 - 8 pm</div>
+							</div>
+							<div id="address">
+								<p>The Burroughes Building</p>
+								<p>639 Queen Street West</p>
+								<p>Toronto, M5V 2B7</p>
+							</div>
+						</div>
+					</article>
+					<article id="right">
+							<!-- Begin MailChimp Signup Form -->
 
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title>Portfolio Show</title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="styles/normalize.min.css">
-    <link rel="stylesheet" href="styles/main.css">
-    <script src="js/script.js"></script>
-</head>
-
-<body>
-    <div class="container">
-        <header>
-            <h1>LOGO</h1>
-        </header>
-        <section>
-            <article id="title">
-                <p>Humber's</p>
-                <h1>Web Design &amp; Interactive Media</h1>
-                <p>Portfolio show</p>
-            </article>
-            <article id="info">
-                <ul>
-                    <li>
-                        <ul id="date">
-                            <li>
-                                <p>April 25th, 2016</p>
-                                <h1>5PM - 8PM</h1>
-                            </li>
-                            <li>
-                                <p>The Burroughes Building 639 Queen Street West Toronto, M5V 2B7</p>
-                            </li>
-                        </ul>
-                    </li>
-                    <li id="newsletter">
-                        Keep an eye out for our full website. Get updates by subscribing below!
-                        <button>NOTIFY ME</button>
-                    </li>
-                </ul>
-            </article>
-    </div>
-    </section>
-    <footer>
-        <ul>
-            <li id="counter">
-                <p>Keep your eyes peeled...</p>
-                COUNTDOWN
-            </li>
-            <li id="social">
-                <ul>
-                    <li>Twitter</li>
-                    <li>Instagram</li>
-                </ul>
-            </li>
-        </ul>
-    </footer>
-</body>
-
+						<div id="mc_embed_signup">
+						<form action="http://jupit3r.us12.list-manage.com/subscribe/post?u=d9486c265684f8c4dd35bb53f&amp;id=f18ada09ed" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+							<div id="mc_embed_signup_scroll">
+							<label for="mce-EMAIL"><p id="label">Keep an eye out for our full website.<br/>
+							Get updates by subscribing below!</p></label>
+							<input type="email" value="" name="EMAIL" class="emailForm" id="mce-EMAIL" placeholder="E-MAIL" required>
+							<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+							<div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_d9486c265684f8c4dd35bb53f_f18ada09ed" tabindex="-1" value=""></div>
+							<div class="clear"><input type="submit" value="NOTIFY ME" name="subscribe" id="mc-embedded-subscribe" class="mailchimp"></div>
+							</div>
+						</form>
+						</div>
+						<!--End mc_embed_signup-->
+					</article>
+				</div>
+			</section>
+			<footer>
+				<div class="container footerBox">
+					<div id="counter">
+						<ul class="countdown">
+							<li><span class="days">00</span><p class="days_text">Days</p></li>
+							<li class="seperator"></li>
+							<li><span class="hours">00</span><p class="hours_text">Hours</p></li>
+							<li class="seperator"></li>
+							<li><span class="minutes">00</span><p class="minutes_text">Minutes</p></li>
+							<li class="seperator"></li>
+							<li><span class="seconds">00</span><p class="seconds_text">Seconds</p></li>
+						</ul>
+					</div>
+					<div id="social">
+						<ul>
+							<li><a href="https://twitter.com/HumberInFlux" target="_blank"><i class="fa fa-twitter fa-3x"></a></i></li>
+							<li><a href="https://www.instagram.com/humberinflux/" target="_blank"><i class="fa fa-instagram fa-3x"></i></a></li>
+						</ul>
+					</div>
+				</div>
+			</footer>
+		</body>
 </html>
--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-"use strict";
+// "usse strict";
+
+jQuery(document).ready(function($) {
+    $('.countdown').downCount({
+        date: '04/25/2016 17:00:00',
+        offset: +10
+    }, function() {
+        alert('WOOT WOOT, done!');
+    });
+});
