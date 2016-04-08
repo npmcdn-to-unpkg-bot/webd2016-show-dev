80dd4a0e7f4906e522f86d09e9b9a50a84de8221
Anna <anna@safronovs.com>
2016-04-08 16:02:15 -0400

    added grunt minify for css and js

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,19 +16,52 @@ grunt.initConfig({
 	    		"styles/main.css":"styles/main.less"
 	    	}	
     	}
-	},
+	  },
     watch: {
       files: ['styles/main.less'],
       tasks: ['default']
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'styles/',
+          src: ['*.css', '!*.min.css'],
+          dest: 'styles/',
+          ext: '.min.css'
+        }]
+      },
+
+      options: {
+        shorthandCompacting: false,
+        roundingPrecision: -1
+      },
+
+      target: {
+        files: {
+          'styles/styles.min.css': ['styles/normalize.min.css', 'styles/grid.min.css', 'styles/main.min.css']
+        }
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'js/script.min.js': ['js/script.js']
+        }
+      }
     }
-	});
+});
 
  // Load the plugin that provides the "uglify" task.
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
-  
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');  
 
   // load default tasks
   grunt.registerTask('default', ['less', 'watch']);
+
 
 };
